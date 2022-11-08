27202985706f9dcf3258ce8bdcfaf36b5c767fc8
RobertMichaelSales <salesmichaelrobert@gmail.com>
2022-11-08 18:00:22 +0000

    Updating to work with  new PyEvtk library

--- NeurComp_SourceCode/Auxiliary Scripts/npy_to_vtk.py
+++ NeurComp_SourceCode/Auxiliary Scripts/npy_to_vtk.py
@@ -1,47 +1,46 @@
 """ Created: 01.06.2022  \\  Updated: 13.06.2022  \\   Author: Robert Sales """
 
 #<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
-def npy2vtk(npy_filename,vtk_filename,size_x,size_y,size_z):
+
+def npy2vtk(npy_filename,dimensions=3):
     
-    # Load data from numpy array
+    
     from numpy import load
-    
-    data = load(npy_filename)
-     
-    x_points = data[:,0].reshape(size_x,size_y,size_z)
-    y_points = data[:,1].reshape(size_x,size_y,size_z)
-    z_points = data[:,2].reshape(size_x,size_y,size_z)
-    volume   = data[:,3].reshape(size_x,size_y,size_z)
-        
-    # Save data to a vts filetype
     from pyevtk.hl import gridToVTK
     
-    # Condition filename
-    vtk_filename = vtk_filename.replace(".vts","")
+    # Load data from numpy array
+    data = load(npy_filename)
     
-    gridToVTK(vtk_filename,x_points,y_points,z_points,pointData={"data": volume})
+    # Create an empty list for coordinate data i.e. [x,y,...,z]
+    coords = []
+     
+    # Extract coordinate data
+    for dim in range(dimensions):coords.append(data[...,dim].flatten())
+        
+    # Extract volume data
+    volume = data[...,3].flatten()
+    
+    # Configure filename
+    vtk_filename = npy_filename.replace(".npy","")
+    
+    # Save to vtk type file
+    gridToVTK(vtk_filename,*coords,pointData={"data":volume})
     
     return None    
 #<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
 import os
 
-parent_filepath = "C:/Users/sales/Documents/Cambridge University/PPD - Project Proposal Dissertation/NeurComp Files/Outputs/Volumes/NeurComp_V6"
-input_filenames = ["difference.npy"] # ["original.npy","normal.npy","tflite.npy","quantd.npy"]
-
-size_x,size_y,size_z = 150,150,150 #49,133,49
+input_filenames = ["/home/rms221/Documents/Compressive_Neural_Representations_Tensorflow/NeurComp_AuxFiles/outputs/neurcomp_1/output_volume.npy"]
 
 # Iterate through all specified files
-for input_filename in input_filenames:
+for input_file in input_filenames:
 
-    # Create absolute path for input and output files
-    file_npy = os.path.join(parent_filepath,input_filename)
-    file_vtk = file_npy.replace(".npy",".vts")
-
-    if os.path.exists(file_npy):
-        print("Converting: '{}' -> '{}'.\n".format(file_npy,file_vtk))
-        npy2vtk(file_npy,file_vtk,size_x,size_y,size_z)
+    if os.path.exists(input_file):
+        print("Converting file: {}".format(input_file))
+        npy2vtk(input_file)
     else: 
-        print("Could not find file '{}'.\n".format(file_npy))
+        print("Could not find file '{}'.\n".format(input_file))
+        
     
 #<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
