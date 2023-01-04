Fixed normalisation with multiple scalars and simplified structure

NeurComp_SourceCode/compress_main.py
@@ -4,11 +4,10 @@
 # Import user-defined libraries 
 
 from data_management         import DataClass
-from file_management         import FileClass
-from network_configuration   import NetworkConfigClass
-from network_encoder         import EncodeWeights,EncodeArchitecture
+from network_configuration   import ConfigurationClass
+from network_encoder         import EncodeParameters,EncodeArchitecture
 from network_model           import ConstructNetwork
-from training_functions      import TrainStep,LearningRate,SignalToNoiseRatio
+from training_utilities      import TrainStep,GetLearningRate,SignalToNoise
 
 #==============================================================================
 # Import libraries and set flags
@@ -22,185 +21,208 @@ import tensorflow as tf
 
 #==============================================================================
     
-def compress(base_directory,input_filepath,config_filepath,verbose):
+def compress(volume_path,config_path,output_path,reconstruct_output):
+    
+    print("-"*80,"\nNEURCOMP: IMPLICIT NEURAL REPRESENTATIONS (Version 2.0)")
         
     #==========================================================================
-    # Enter the main script and check for hardware acceleration
-    print("-"*80,"\nNEURCOMP: IMPLICIT NEURAL REPRESENTATIONS (Version 2.0)")
-    
+    # Check whether hardware acceleration is enabled
+   
     gpus = tf.config.list_physical_devices('GPU')
     
-    if (len(gpus) == 0):        
-        print("\n{:30}{} - {}".format("CUDA GPU(s) Detected:",len(gpus),"Hardware Acceleration Is Disabled"))
-    else:        
-        tf.config.experimental.set_memory_growth(gpus[0],True)
+    if (len(gpus) != 0): 
         print("\n{:30}{} - {}".format("CUDA GPU(s) Detected:",len(gpus),"Hardware Acceleration Is Enabled"))
+        tf.config.experimental.set_memory_growth(gpus[0],True)
+    else:        
+        print("\n{:30}{} - {}".format("CUDA GPU(s) Detected:",len(gpus),"Hardware Acceleration Is Disabled"))
+        return None
         
     #==========================================================================
-    # Start initialising I/O data files
+    # Initialise i/o 
+    
     print("-"*80,"\nINITIALISING DATA I/O:")
     
-    # Create 'DataClass' objecta to store input/output data
-    input_data, output_data = DataClass(), DataClass()
+    # Create 'DataClass' objects to store i/o data
+    i_data, o_data = DataClass(), DataClass()
     
     # Load and normalise input data
-    input_data.LoadData(input_filepath=input_filepath,i_dimensions=3,o_dimensions=1)
+    i_data.LoadData(volume_path=volume_path,i_dimensions=3,o_dimensions=1,normalise=True)
     
     # Copy meta-data from the input
-    output_data.CopyData(DataClassObject=input_data)
+    o_data.CopyMetaData(DataClassObject=i_data)
     
     #==========================================================================
-    # Configure network model and build
-    print("-"*80,"\nCONFIGURING BUILD:")
+    # Configure network 
     
-    # Create a 'NetworkConfigClass' object to store net parameters 
-    network_config = NetworkConfigClass(config_filepath=config_filepath)
+    print("-"*80,"\nCONFIGURING NETWORK:")
+    
+    # Create a 'ConfigurationClass' object to store net parameters 
+    network_cfg = ConfigurationClass(config_path=config_path)
     
     # Generate the network structure based on the input dimensions
-    network_config.NetworkStructure(input_data=input_data)
+    network_cfg.GenerateStructure(i_dimensions=i_data.i_dimensions,o_dimensions=i_data.o_dimensions,i_size=i_data.i_size)
     
-    # Create a 'FileClass'object to store output directory and filepath data
-    print("\n{:30}{}".format("Created filepaths at:",base_directory.split("/")[-1]))
-    filepaths = FileClass(base_directory=base_directory,network_config=network_config)
-        
     # Build NeurComp from the config information
-    print("\n{:30}{}".format("Constructed network:",network_config.network_name))
-    neur_comp = ConstructNetwork(layer_dimensions=network_config.layer_dimensions)
+    SquashNet = ConstructNetwork(layer_dimensions=network_cfg.layer_dimensions)
     
-    # Plot and save an image of the network architecture#
-    tf.keras.utils.plot_model(neur_comp,to_file=filepaths.network_image_path,show_shapes=True)
+    # Set a training optimiser
+    optimiser = tf.keras.optimizers.Adam(learning_rate=network_cfg.initial_lr)
     
-    # Set a training optimiser and select a training metric 
-    optimiser = tf.keras.optimizers.Adam(learning_rate=network_config.initial_learning_rate)
-    mse_error = tf.keras.metrics.MeanSquaredError()
+    # Set a performance metric
+    mse_error_metric = tf.keras.metrics.MeanSquaredError()
     
     #==========================================================================
-    # Configure network model and build
+    # Configure output folder
+    print("-"*80,"\nCONFIGURING FOLDERS:")
+    
+    # Create an output directory for all future saved files
+    output_directory = os.path.join(output_path,network_cfg.network_name)
+    if not os.path.exists(output_directory):os.makedirs(output_directory)
+    print("\n{:30}{}".format("Created output folder:",output_directory.split("/")[-1]))
+            
+    #==========================================================================
+    # Configure dataset
     print("-"*80,"\nCONFIGURING DATASET:")
     
     # Generate a TF dataset to supply volume and values batches during training 
-    training_dataset = input_data.MakeDataset(batch_size=network_config.batch_size,repeat=False)
+    dataset = i_data.MakeDataset(batch_size=network_cfg.batch_size,repeat=False)
     
     #==========================================================================
-    # Start compressing data
+    # Compression loop
     print("-"*80,"\nCOMPRESSING DATA:")
     
-    # Create a dictionary of lists for storing training data
+    # Create a dictionary of lists to store training data
     training_data = {"epoch": [],"error": [],"time": [],"learning_rate": []}
-    
-    # Start the training timer
+
+    # Start the overall training timer
     training_time_tick = time.time()
     
-    # Enter the outer training loop: epochs:
-    for epoch in range(network_config.epochs):
+    # Iterate through each epoch
+    for epoch in range(network_cfg.epochs):
         
         print("\n",end="")
         
-        # Store the current epoch number
+        # Store and print the current epoch number
         training_data["epoch"].append(float(epoch))
-          
-        # Determine and store the learning rate 
-        learning_rate = GetLearningRate(network_config=network_config,epoch=epoch)
-        training_data["learning_rate"].append(float(learning_rate))   
+        print("{:30}{:02}/{:02}".format("Epoch:",epoch,network_cfg.epochs))
         
-        # Assign the learning rate to the optimiser
+        # Determine, update, store and print the learning rate 
+        learning_rate = GetLearningRate(initial_lr=network_cfg.initial_lr,half_life=network_cfg.half_life,epoch=epoch)
         optimiser.lr.assign(learning_rate)
-        
-        # Print the current epoch number and learning rate
-        print("{:30}{:02}/{:02}".format("Epoch:",epoch,network_config.epochs))
+        training_data["learning_rate"].append(float(learning_rate))   
         print("{:30}{:.3E}".format("Learning Rate:",learning_rate))
         
-        # Start timing epoch
+        # Start timing current epoch
         epoch_time_tick = time.time()
         
-        
-        # Enter the inner training loop: batches:
-        for batch, (volume_batch,values_batch) in enumerate(training_dataset):
+        ## Iterate through each batch
+        for batch, (volume_batch,values_batch) in enumerate(dataset):
             
-            # Print the batch number
-            if verbose: print("\r{:30}{:04}/{:04}".format("Batch Number:",(batch+1),len(training_dataset)),end="")
+            # Print the current batch number 
+            print("\r{:30}{:04}/{:04}".format("Batch Number:",(batch+1),len(dataset)),end="")
+            
+            # Run a training step 
+            TrainStep(model=SquashNet,optimiser=optimiser,metric=mse_error_metric,volume_batch=volume_batch,values_batch=values_batch)
+        ##
         
-            # Run a training step with the current batch
-            TrainStep(model=neur_comp,optimiser=optimiser,metric=mse_error,volume_batch=volume_batch,values_batch=values_batch)
-                
-         
-        if verbose: print("\n",end="")
+        print("\n",end="")
         
         # End the epoch time and store the elapsed time 
-        epoch_time_tock = time.time()       
-        training_data["time"].append(float(epoch_time_tock-epoch_time_tick))
+        epoch_time_tock = time.time() 
+        epoch_time = float(epoch_time_tock-epoch_time_tick)
+        training_data["time"].append(epoch_time)
+        print("{:30}{:.2f} seconds".format("Epoch Time:",epoch_time))
         
         # Fetch, store and reset and the training error
-        training_data["error"].append(float(mse_error.result().numpy()))
-        mse_error.reset_states()
-    
-        # Print the mean squared error and training time
-        print("{:30}{:.7f}".format("Mean Squared Error:",training_data["error"][-1]))
-        print("{:30}{:.2f} seconds".format("Epoch Time:",training_data["time"][-1]))
+        mse_error = float(mse_error_metric.result().numpy())
+        mse_error_metric.reset_states()
+        training_data["error"].append(mse_error)
+        print("{:30}{:.7f}".format("Mean Squared Error:",mse_error))
+    ##   
  
-    # End the training timer
+    # End the overall training timer
     training_time_tock = time.time()
-    print("\n{:30}{:.2f} seconds".format("Training Duration:",training_time_tock-training_time_tick))    
+    training_time = float(training_time_tock-training_time_tick)
+    print("\n{:30}{:.2f} seconds".format("Training Duration:",training_time))    
     
     #==========================================================================
-    # Save training data
-    print("-"*80,"\nSAVING NETWORK AND RESULTS:")
+    # Save results
+    print("-"*80,"\nSAVING RESULTS:")
     
-    # Save the configuration
-    print("\n{:30}{}".format("Saved configuration to:",filepaths.network_configuration_path.split("/")[-1]))
-    with open(filepaths.network_configuration_path,"w") as file: json.dump(vars(network_config),file,indent=4)
+    print("\n",end="")
     
     # Save the training data
-    print("{:30}{}".format("Saved training data to:",filepaths.training_data_path.split("/")[-1]))
-    with open(filepaths.training_data_path,"w") as file: json.dump(training_data,file,indent=4,sort_keys=True)
+    training_data_path = os.path.join(output_path,network_cfg.network_name,"training_data.json")
+    with open(training_data_path,"w") as file: json.dump(training_data,file,indent=4,sort_keys=True)
+    print("{:30}{}".format("Saved training data to:",training_data_path.split("/")[-1]))
+
+    # Save the configuration
+    configuration_path = os.path.join(output_path,network_cfg.network_name,"configuration.json")
+    with open(configuration_path,"w") as file: json.dump(vars(network_cfg),file,indent=4)
+    print("{:30}{}".format("Saved configuration to:",configuration_path.split("/")[-1]))
     
-    # Save the net architecture
-    print("{:30}{}".format("Saved architecture to:",filepaths.network_architecture_path.split("/")[-1]))
-    EncodeArchitecture(config=network_config,filepath=filepaths.network_architecture_path)
+    #==========================================================================
+    # Save network 
+    print("-"*80,"\nSAVING NETWORK:")
     
-    # Save the trained weights
-    print("{:30}{}".format("Saved weights/biases to:",filepaths.network_weights_path.split("/")[-1]))
-    EncodeWeights(network=neur_comp,filepath=filepaths.network_weights_path)
+    print("\n",end="")
+    
+    # Save the parameters
+    parameters_path = os.path.join(output_path,network_cfg.network_name,"parameters.bin")
+    EncodeParameters(network=SquashNet,parameters_path=parameters_path)
+    print("{:30}{}".format("Saved parameters to:",parameters_path.split("/")[-1]))
+    
+    # Save the architecture
+    architecture_path = os.path.join(output_path,network_cfg.network_name,"architecture.bin")
+    EncodeArchitecture(network_cfg=network_cfg,filepath=architecture_path)
+    print("{:30}{}".format("Saved architecture to:",architecture_path.split("/")[-1]))
+    
+    # Save an image of the network architecture
+    network_plot_path = os.path.join(output_path,network_cfg.network_name,"network_plot.png")
+    tf.keras.utils.plot_model(model=SquashNet,to_file=network_plot_path,show_shapes=True)
+    print("{:30}{}".format("Saved network plot to:",network_plot_path.split("/")[-1]))
+
+    if not reconstruct_output:
+        print("-"*80,"\n")
+        return None
+    else: pass
     
     #==========================================================================
     # Start predicting data
-    print("-"*80,"\nRECONSTRUCTING INPUT:")
+    print("-"*80,"\nSAVING OUTPUTS:")
     
-    # Predict values using the Neurcomp's learned weights and biases
-    output_data.flat_values = neur_comp.predict(output_data.flat_volume,batch_size=network_config.batch_size,verbose="1")
-    output_data.values = np.reshape(output_data.flat_values,(output_data.volume.shape[:-1]+(1,)),order="C")
-    
-    # Compute the peak signal-to-noise ratio of the predicted volume
-    psnr = SignalToNoiseRatio(true=input_data.values,pred=output_data.values)
-    print("\n{:30}{:.3f}".format("Output PSNR:",psnr))
-    
-    # Save the predicted values to a '.npy' volume rescaling as appropriate
-    print("{:30}{}".format("Saved reconstruction to:",filepaths.output_volume_path.split("/")[-1]))
-    output_data.SaveData(output_volume_path=filepaths.output_volume_path,reverse_normalise=True)
+    print("\n",end="")
+
+    # Generate the output volume and calculate the PSNR
+    o_data.flat_values = SquashNet.predict(o_data.flat_volume,batch_size=network_cfg.batch_size,verbose="1")
+    o_data.values = np.reshape(o_data.flat_values,(o_data.volume.shape[:-1]+(1,)),order="C")
+    print("{:30}{:.3f}".format("Output volume PSNR:",SignalToNoise(true=i_data.values,pred=o_data.values)))
+
+    # Save the output volume to ".npy" and ".vtk" files
+    output_volume_path = os.path.join(output_path,network_cfg.network_name,"output_volume")
+    o_data.SaveData(output_volume_path=output_volume_path,reverse_normalise=True)
+    print("{:30}{}".format("Saved output volume to:",output_volume_path.split("/")[-1]))
     
     #==========================================================================
-    print("-"*80,"\n"*2)
+    print("-"*80,"\n")
     
     return None
-    
+   
 #==============================================================================
 # Define the script to run when envoked from the terminal
 
 if __name__=="__main__":
-
+    
     # Set config filepath
-    config_filepath = "/home/rms221/Documents/Compressive_Neural_Representations_Tensorflow/NeurComp_AuxFiles/inputs/configs/config_test.json"
-    
-    # Set base directory
-    base_directory = "/home/rms221/Documents/Compressive_Neural_Representations_Tensorflow/NeurComp_AuxFiles"
-    
+    config_path = "/home/rms221/Documents/Compressive_Neural_Representations_Tensorflow/NeurComp_AuxFiles/inputs/configs/config_test.json"
+       
     # Set input filepath
-    input_filepath = "/home/rms221/Documents/Compressive_Neural_Representations_Tensorflow/NeurComp_AuxFiles/inputs/volumes/test_vol.npy"
- 
-    # compress(base_directory=base_directory,input_filepath=input_filepath,config_filepath=config_filepath,verbose=True)   
-
-else:
+    volume_path = "/home/rms221/Documents/Compressive_Neural_Representations_Tensorflow/NeurComp_AuxFiles/inputs/volumes/test_vol.npy"
     
-    print("Please invoke 'compress_main.py' correctly from the terminal!")
+    # Set output filepath
+    output_path = "/home/rms221/Documents/Compressive_Neural_Representations_Tensorflow/NeurComp_AuxFiles/outputs"
+ 
+    # Execute compression
+    data = compress(volume_path=volume_path,config_path=config_path,output_path=output_path,reconstruct_output=True)   
     
 #==============================================================================

NeurComp_SourceCode/data_management.py
@@ -30,32 +30,32 @@ class DataClass():
     
     # -> Note: Make sure the flattening takes place after normalisation
     
-    def LoadData(self,input_filepath,i_dimensions,o_dimensions,normalise=True):
+    def LoadData(self,volume_path,i_dimensions,o_dimensions,normalise=True):
                 
-        print("\n{:30}{}".format("Loaded data:",input_filepath.split("/")[-1]))
+        print("\n{:30}{}".format("Loaded data:",volume_path.split("/")[-1]))
     
         # Set the input and output dimensions defined by user input
         self.i_dimensions = i_dimensions
         self.o_dimensions = o_dimensions
     
         # Load the data then extract the volume and values matrices
-        data = np.load(input_filepath)    
+        data = np.load(volume_path)    
         self.volume = data[...,:self.i_dimensions]
         self.values = data[...,self.i_dimensions:]
         
-        # Determine the input resolution and count of scalar values
+        # Determine the input resolution and number of input values
         self.resolution = self.values.shape[:-1]  
-        self.input_size = self.values.size
+        self.i_size = self.values.size
         
         # Determine the maximum, minimum, average and range of 'volume'
-        self.volume_max = np.amax(self.volume,axis=tuple(np.arange(self.i_dimensions)))
-        self.volume_min = np.amin(self.volume,axis=tuple(np.arange(self.i_dimensions)))
+        self.volume_max = np.array([self.volume[...,i].max() for i in np.arange(self.i_dimensions)])
+        self.volume_min = np.array([self.volume[...,i].min() for i in np.arange(self.i_dimensions)])
         self.volume_avg = (self.volume_max+self.volume_min)/2.0
         self.volume_rng = abs(self.volume_max-self.volume_min)
         
         # Determine the maximum, minimum, average and range of 'values'
-        self.values_max = np.amax(self.values,axis=tuple(np.arange(self.o_dimensions)))
-        self.values_min = np.amin(self.values,axis=tuple(np.arange(self.o_dimensions)))
+        self.values_max = np.array([self.values[...,i].max() for i in np.arange(self.o_dimensions)])
+        self.values_min = np.array([self.values[...,i].min() for i in np.arange(self.o_dimensions)])
         self.values_avg = (self.values_max+self.values_min)/2.0
         self.values_rng = abs(self.values_max-self.values_min)
         
@@ -64,6 +64,10 @@ class DataClass():
             self.volume = 2.0*((self.volume-self.volume_avg)/(self.volume_rng))        
             self.values = 2.0*((self.values-self.values_avg)/(self.values_rng))
         else: pass
+    
+        # Flatten 'volume' and 'values' into equal lists of vectors
+        self.flat_volume = np.reshape(np.ravel(self.volume,order="C"),(-1,self.volume.shape[-1]))
+        self.flat_values = np.reshape(np.ravel(self.values,order="C"),(-1,self.values.shape[-1]))
                 
         return None
        
@@ -74,7 +78,7 @@ class DataClass():
     
     # -> Note: 'getattr()' and 'setattr()' are used to copy without referencing 
     
-    def CopyData(self,DataClassObject):
+    def CopyMetaData(self,DataClassObject):
         
         # Extract attribute keys from 'DataObject' and define exceptions
         exception_keys = ["values","flat_values"]
@@ -95,10 +99,10 @@ class DataClass():
     
     # -> Note: 'volume' and 'values' must be reshaped to match the input shapes
     
-    def SaveData(self,output_volume_path,normalise=True):
+    def SaveData(self,output_volume_path,reverse_normalise=True):
                     
         # Reverse normalise 'volume' and 'values' to the initial ranges
-        if normalise:
+        if reverse_normalise:
             self.volume = ((self.volume_rng*(self.volume/2.0))+self.volume_avg)
             self.values = ((self.values_rng*(self.values/2.0))+self.values_avg)
         else: pass
@@ -107,7 +111,7 @@ class DataClass():
         np.save(output_volume_path,np.concatenate((self.volume,self.values),axis=-1))
         
         # Save as VTK file
-        volume_list = [np.ascontiguousarray(self.volume[...,x]) for x in range(self.input_dimensions)]
+        volume_list = [np.ascontiguousarray(self.volume[...,x]) for x in range(self.i_dimensions)]
         values_dict = {"values":np.ascontiguousarray(self.values[...,0])}
         gridToVTK(output_volume_path,*volume_list,pointData=values_dict)
 
@@ -126,10 +130,6 @@ class DataClass():
         
         print("\n{:30}{}{}".format("Made TF dataset:","batch_size = ",batch_size))
         
-        # Flatten 'volume' and 'values' into equal lists of vectors
-        self.flat_volume = np.reshape(np.ravel(self.volume,order="C"),(-1,self.volume.shape[-1]))
-        self.flat_values = np.reshape(np.ravel(self.values,order="C"),(-1,self.values.shape[-1])) 
-
         # Create a dataset whose elements are slices of the given tensors
         dataset = tf.data.Dataset.from_tensor_slices((self.flat_volume,self.flat_values))
         
@@ -142,10 +142,10 @@ class DataClass():
         else: pass
         
         # Randomly shuffle the elements of the cached dataset 
-        dataset = dataset.shuffle(buffer_size=self.input_size,reshuffle_each_iteration=True)
+        dataset = dataset.shuffle(buffer_size=self.i_size,reshuffle_each_iteration=True)
                     
         # Concatenate elements of the dataset into mini-batches
-        dataset = dataset.batch(batch_size=batch_size,drop_remainder=True)
+        dataset = dataset.batch(batch_size=batch_size,drop_remainder=False)
         
         # Pre-fetch elements from the dataset to increase throughput
         dataset = dataset.prefetch(buffer_size=tf.data.AUTOTUNE)

NeurComp_SourceCode/network_encoder.py
@@ -14,14 +14,14 @@ import numpy as np
 
 # Note: The np.method '.tobytes()' returns the same bytestring as 'struct.pack'
 
-def EncodeWeights(network,filepath):
+def EncodeParameters(network,parameters_path):
     
     # Extract a sorted list of the names of each layer in the network
     layer_names = network.get_weight_paths().keys()
     layer_names = sorted(list(layer_names),key=SortLayerNames)
     
     # Open the weights file in 'write as binary' mode
-    file = open(filepath,"wb")
+    file = open(parameters_path,"wb")
     
     # Iterate through each of the network layers, in order 
     for layer_name in layer_names: 
@@ -48,10 +48,10 @@ def EncodeWeights(network,filepath):
 # Define a function to encode the network layer dimensions (or architecture) as
 # a binary file containing strings of bytes
 
-def EncodeArchitecture(config,filepath):
+def EncodeArchitecture(network_cfg,filepath):
 
     # Extract the list of network layer dimensions
-    layer_dimensions = np.array(config.layer_dimensions).astype(np.uint16)
+    layer_dimensions = np.array(network_cfg.layer_dimensions).astype(np.uint16)
     
     # Open the architecture file in 'write as binary' mode
     file = open(filepath,"wb")

NeurComp_SourceCode/network_model.py
@@ -9,23 +9,97 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import numpy as np
 import tensorflow as tf
 
+'''
 #==============================================================================
 # Define a 'Sine Layer' 
 
-def SineLayer(inputs,units,name):
+def OLDSineLayer(inputs,units,scale,name):
     
-    x = tf.keras.layers.Dense(units=units,name=name+"_dense")(inputs)
-    x = tf.math.sin(x)
+    # x1 = sin(W1*x0 + b1)
+    
+    x = tf.keras.layers.Dense(units=units,activation=None,use_bias=True,kernel_initializer="glorot_uniform",bias_initializer="zeros",name=name+"_dense")(inputs)
+    x = tf.math.sin(tf.math.multiply(x,scale))
     
     return x
     
 #==============================================================================
 # Define a 'Sine Block'
 
-def SineBlock(inputs,units,name):
+def OLDSineBlock(inputs,units,scale,name,avg_1,avg_2):
+    
+    # x1 = (1/2) * (x0 + sin(w12*sin(w11*x0 + b11) + b12))
             
+    weight_1 = tf.constant(0.5) if avg_1 else tf.constant(1.0)  # Weight 1
+    weight_2 = tf.constant(0.5) if avg_2 else tf.constant(1.0)  # Weight 2
+        
+    sine_1 = tf.math.sin(tf.math.multiply(tf.keras.layers.Dense(units=units,activation=None,use_bias=True,kernel_initializer="glorot_uniform",bias_initializer="zeros",name=name+"_dense_a")(inputs*weight_1),scale))
+    sine_2 = tf.math.sin(tf.math.multiply(tf.keras.layers.Dense(units=units,activation=None,use_bias=True,kernel_initializer="glorot_uniform",bias_initializer="zeros",name=name+"_dense_b")(sine_1)         ,scale))
+    
+    x = tf.math.add(tf.math.multiply(inputs,weight_2),sine_2)
+    
+    return x
+
+#==============================================================================
+# Define a function that constructs the 'SIREN' network 
+
+def OLDConstructNetwork(layer_dimensions):
+    
+    total_layers = len(layer_dimensions)
+    
+    scale = tf.constant(1.0)
+
+    for layer in np.arange(total_layers):
+          
+        # Add the input layer and the first sine layer
+        if (layer == 0):                  
+          
+            input_layer = tf.keras.layers.Input(shape=(layer_dimensions[layer],),name="l{}_input".format(layer))
+            x = SineLayer(inputs=input_layer,units=layer_dimensions[layer+1],scale=scale,name="l{}_sinelayer".format(layer))
+          
+        # Add the final output layer
+        elif (layer == (total_layers - 1)):
+          
+            output_layer =  tf.keras.layers.Dense(units=layer_dimensions[layer],name="l{}_output".format(layer))(x)
+          
+        # Add the intermediate sine blocks
+        else:
+            
+            avg_1 = False # (layer > 1)
+            avg_2 = False #(layer == (total_layers - 2))           # this used to be 3
+        
+            x = SineBlock(inputs=x,units=layer_dimensions[layer],scale=scale,avg_1=avg_1,avg_2=avg_2,name="l{}_sineblock".format(layer))
+    
+    # Create the network model
+    NeurComp = tf.keras.Model(inputs=input_layer,outputs=output_layer)
+    
+    return NeurComp
+
+#==============================================================================
+'''
+
+
+#==============================================================================
+# Define a 'Sine Layer' 
+
+def SineLayer(inputs,units,name):
+    
+    # x1 = sin(W1*x0 + b1)
+    
+    x = tf.keras.layers.Dense(units=units,name=name+"_dense")(inputs)
+    x = tf.math.sin(x)
+    
+    return x
+
+#==============================================================================
+# Define a 'Sine Block'
+
+def SineBlock(inputs,units,name):
+    
+    # x1 = (1/2) * (x0 + sin(w12*sin(w11*x0 + b11) + b12))
+        
     sine_1 = tf.math.sin(tf.keras.layers.Dense(units=units,name=name+"_dense_a")(inputs))
     sine_2 = tf.math.sin(tf.keras.layers.Dense(units=units,name=name+"_dense_b")(sine_1))
+    
     x = tf.math.add(inputs,sine_2)
     
     return x
@@ -60,4 +134,4 @@ def ConstructNetwork(layer_dimensions):
     
     return NeurComp
 
-#=============================================================================
+#==============================================================================