b036eb5bd3f864a25151e56eb6c5ac0dfc58d1a6
RobertMichaelSales <salesmichaelrobert@gmail.com>
2023-01-19 16:59:54 +0000

    Added stylesheet for plotting

--- NeurComp_SourceCode/Auxiliary_Scripts/plot_results.py
+++ NeurComp_SourceCode/Auxiliary_Scripts/plot_results.py
@@ -155,7 +155,7 @@ c_map = cm.get_cmap("Blues",20)
 
 #==============================================================================
 
-def PlotMSEandTimevsBatchSize(save=False):
+def PlotVersusBatchSize(save=False):
     
     matplotlib.rcParams.update(params_plot)   
     
@@ -168,86 +168,93 @@ def PlotMSEandTimevsBatchSize(save=False):
     # Set current directory
     plot_directory = "/home/rms221/Documents/Compressive_Neural_Representations_Tensorflow/NeurComp_AuxFiles/plots"
     
-    # Set input filepath
-    input_volume_filepath = "/home/rms221/Documents/Compressive_Neural_Representations_Tensorflow/NeurComp_AuxFiles/inputs/volumes/passage.npy"
-    
-    # Get a sorted list of all the experiements directories
-    experiment_directories = sorted(os.listdir(base_directory),key = lambda x: int(x.split("_")[-1]))
-        
-    #==========================================================================
-    # Set up storage for results
-    
-    # Record once per experiment
-    target_compression_ratio = []
-    actual_compression_ratio = []
-    initial_lr = []
-    batch_size = []
-    
-    training_duration = []
-    mean_squared_loss = []
-    # peak_signal_noise = []
-    
-    #==========================================================================
+    #======================================================================
     # Make figure and plot axes
     fig, ax1 = plt.subplots(1,1,constrained_layout=True)
     
     ax2 = ax1.twinx()
     
     #==========================================================================
-    # Iterate through all the experiments
-    for experiment_directory in experiment_directories:
+    # Iterate through each data set
+    for dataset in ["cube","passage"]:
+    
+        # Set input filepath
+        input_volume_filepath = "/home/rms221/Documents/Compressive_Neural_Representations_Tensorflow/NeurComp_AuxFiles/inputs/volumes/" + dataset + ".npy"
         
-        # Set the current experiment directory
-        experiment_directory = os.path.join(base_directory,experiment_directory)
-        
-        # Set the config, output, and training data filepaths
-        configuration_filepath = os.path.join(experiment_directory,"configuration.json")
-        output_volume_filepath = os.path.join(experiment_directory,"output_volume.npy")
-        training_data_filepath = os.path.join(experiment_directory,"training_data.json")
-        
-        # Load the config and training data 
-        with open(configuration_filepath) as configuration_file: configuration = json.load(configuration_file)
-        with open(training_data_filepath) as training_data_file: training_data = json.load(training_data_file)
-        
-        # Load the input and output volumes
-        input_data = np.load(input_volume_filepath)
-        output_data = np.load(output_volume_filepath)
-        
+        # Get a sorted list of all the experiements directories
+        experiment_directories = [x for x in os.listdir(base_directory) if dataset in x]
+        experiment_directories.sort(key = lambda x: float(x.split("_")[-2]))
+                    
         #======================================================================
-        # Extract data for plotting
+        # Set up storage for results
         
         # Record once per experiment
-        target_compression_ratio.append(configuration["target_compression_ratio"])
-        actual_compression_ratio.append(configuration["actual_compression_ratio"])
-        initial_lr.append(configuration["initial_lr"])
-        batch_size.append(configuration["batch_size"])
+        target_compression_ratio = []
+        actual_compression_ratio = []
+        initial_lr = []
+        batch_size = []
+        batch_fraction = []
         
-        # Record every training epoch
-        learning_rate = training_data["learning_rate"]
-        epoch = training_data["epoch"]
-        error = training_data["error"]
-        time = training_data["time"]
-        
-        # Derived data from training
-        training_duration.append(np.sum(time))
-        mean_squared_loss.append(error[-1])   
-        # peak_signal_noise.append(training_data["psnr"][-1])
-        
-        #======================================================================
-        # Specific data wrangling
-        
-        mean_squared_loss_percentage = 100 * np.array(mean_squared_loss)
-        
-        batch_size_base2 = [math.log2(x) for x in batch_size]
+        training_duration = []
+        mean_squared_loss = []
+        peak_signal_noise = []
                 
-    #======================================================================
-    # Matplotlib code
+        #======================================================================
+        # Iterate through all the experiments
+        for experiment_directory in experiment_directories:
+            
+            print(experiment_directory)
+            
+            # Set the current experiment directory
+            experiment_directory = os.path.join(base_directory,experiment_directory)
+            
+            # Set the config, output, and training data filepaths
+            configuration_filepath = os.path.join(experiment_directory,"configuration.json")
+            output_volume_filepath = os.path.join(experiment_directory,"output_volume.npy")
+            training_data_filepath = os.path.join(experiment_directory,"training_data.json")
+            
+            # Load the config and training data 
+            with open(configuration_filepath) as configuration_file: configuration = json.load(configuration_file)
+            with open(training_data_filepath) as training_data_file: training_data = json.load(training_data_file)
+            
+            # Load the input and output volumes
+            input_data = np.load(input_volume_filepath)
+            output_data = np.load(output_volume_filepath)
+            
+            #==================================================================
+            # Extract data for plotting
+            
+            # Record once per experiment
+            target_compression_ratio.append(configuration["target_compression_ratio"])
+            actual_compression_ratio.append(configuration["actual_compression_ratio"])
+            initial_lr.append(configuration["initial_lr"])
+            batch_size.append(configuration["batch_size"])
+            batch_fraction.append(configuration["batch_fraction"])
+            
+            # Record every training epoch
+            learning_rate = training_data["learning_rate"]
+            epoch = training_data["epoch"]
+            error = training_data["error"]
+            time = training_data["time"]
+            
+            # Derived data from training
+            training_duration.append(np.sum(time))
+            mean_squared_loss.append(error[-1])   
+            peak_signal_noise.append(training_data["psnr"][-1])
+            
+            #==================================================================
+            # Specific data wrangling
+            
+            mean_squared_loss_percentage = 100 * np.array(mean_squared_loss)
+                    
+        #======================================================================
+        # Matplotlib code
+            
+        ax1.scatter(batch_fraction,mean_squared_loss_percentage,marker="s",color=colours["blue"])
+        ax1.plot(batch_fraction,mean_squared_loss_percentage,linestyle="solid",color=colours["blue"])
         
-    ax1.scatter(batch_size,mean_squared_loss_percentage,marker="s",color=colours["blue"])
-    ax1.plot(batch_size,mean_squared_loss_percentage,linestyle="dotted",color=colours["blue"])
-    
-    ax2.scatter(batch_size,training_duration,marker="s",color=colours["red"])
-    ax2.plot(batch_size,training_duration,linestyle="dotted",color=colours["red"])
+        ax2.scatter(batch_fraction,training_duration,marker="s",color=colours["red"])
+        ax2.plot(batch_fraction,training_duration,linestyle="solid",color=colours["red"])
 
     # Set x- and y-ticks
     # ax1.set_xticks(,minor=False)
@@ -260,7 +267,7 @@ def PlotMSEandTimevsBatchSize(save=False):
     # ax2.set_ylim()
     
     # # Set x- and y- scale
-    ax1.set_xscale('log',base=2)
+    ax1.set_xscale('linear')#'log',base=10)
     ax1.set_yscale('linear')
     ax2.set_yscale('linear')
 
@@ -285,130 +292,5 @@ def PlotMSEandTimevsBatchSize(save=False):
 
 #==============================================================================
 
-def PlotMSEvsNormalisedBatchSize(save=False):
-    
-    matplotlib.rcParams.update(params_plot)   
-    
-    #==========================================================================
-    # Set filepaths and directories
-    
-    # Set base directory
-    base_directory = "/home/rms221/Documents/Compressive_Neural_Representations_Tensorflow/NeurComp_AuxFiles/outputs"
-    
-    # Set current directory
-    plot_directory = "/home/rms221/Documents/Compressive_Neural_Representations_Tensorflow/NeurComp_AuxFiles/plots"
-    
-    # Set input filepath
-    input_volume_filepath = "/home/rms221/Documents/Compressive_Neural_Representations_Tensorflow/NeurComp_AuxFiles/inputs/volumes/passage.npy"
-    
-    # Get a sorted list of all the experiements directories
-    experiment_directories = sorted(os.listdir(base_directory),key = lambda x: int(x.split("_")[-1]))
-        
-    #==========================================================================
-    # Set up storage for results
-    
-    # Record once per experiment
-    target_compression_ratio = []
-    actual_compression_ratio = []
-    initial_lr = []
-    batch_size = []
-    
-    training_duration = []
-    mean_squared_loss = []
-    # peak_signal_noise = []
-    
-    #==========================================================================
-    # Make figure and plot axes
-    fig, ax1 = plt.subplots(1,1,constrained_layout=True)
-    
-    # ax2 = ax1.twinx()
-    
-    #==========================================================================
-    # Iterate through all the experiments
-    for experiment_directory in experiment_directories:
-        
-        # Set the current experiment directory
-        experiment_directory = os.path.join(base_directory,experiment_directory)
-        
-        # Set the config, output, and training data filepaths
-        configuration_filepath = os.path.join(experiment_directory,"configuration.json")
-        output_volume_filepath = os.path.join(experiment_directory,"output_volume.npy")
-        training_data_filepath = os.path.join(experiment_directory,"training_data.json")
-        
-        # Load the config and training data 
-        with open(configuration_filepath) as configuration_file: configuration = json.load(configuration_file)
-        with open(training_data_filepath) as training_data_file: training_data = json.load(training_data_file)
-        
-        # Load the input and output volumes
-        input_data = np.load(input_volume_filepath)
-        output_data = np.load(output_volume_filepath)
-        
-        #======================================================================
-        # Extract data for plotting
-        
-        # Record once per experiment
-        target_compression_ratio.append(configuration["target_compression_ratio"])
-        actual_compression_ratio.append(configuration["actual_compression_ratio"])
-        initial_lr.append(configuration["initial_lr"])
-        batch_size.append(configuration["batch_size"])
-        
-        # Record every training epoch
-        learning_rate = training_data["learning_rate"]
-        epoch = training_data["epoch"]
-        error = training_data["error"]
-        time = training_data["time"]
-        
-        # Derived data from training
-        training_duration.append(np.sum(time))
-        mean_squared_loss.append(error[-1])   
-        # peak_signal_noise.append(training_data["psnr"][-1])
-        
-        #======================================================================
-        # Specific data wrangling
-        
-        mean_squared_loss_percentage = 100 * np.array(mean_squared_loss)
-        
-        size = configuration["size"]
-        
-        batch_size_normalised = [(x/size) for x in batch_size]
-                
-    #======================================================================
-    # Matplotlib code
-        
-    ax1.scatter(batch_size_normalised,mean_squared_loss_percentage,marker="s",color=colours["blue"])
-    ax1.plot(batch_size_normalised,mean_squared_loss_percentage,linestyle="dotted",color=colours["blue"])
-
-    # Set x- and y-ticks
-    # ax1.set_xticks(,minor=False)
-    # ax1.set_yticks(,minor=False)
-    
-    # # Set x- and y-limits
-    # ax1.set_xlim()
-    # ax1.set_ylim()
-    
-    # # Set x- and y- scale
-    ax1.set_xscale('log')
-    ax1.set_yscale('linear')
-
-    # Set x- and y-labels
-    ax1.set_xlabel(r"Batch Size / Input Field Size")
-    ax1.set_ylabel(r"Mean-Squared Error / %",color=colours["blue"])
-
-    # Set the figure title
-    ax1.set_title("Batch Size vs. Mean-Squared Error vs. Training Duration")
-
-    if save:
-        savename = os.path.join(plot_directory,"")
-        plt.savefig(savename)
-    else: pass
-
-    plt.show()
-        
-    #==========================================================================
-            
-    return None
-
-#==============================================================================
-
 # PlotMSEandTimevsBatchSize()
-PlotMSEvsNormalisedBatchSize()
+PlotVersusBatchSize()
