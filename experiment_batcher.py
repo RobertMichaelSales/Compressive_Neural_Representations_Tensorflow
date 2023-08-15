e0a0963b72efb3a0a5c5ba0c69445d581aac27dd
RobertMichaelSales <salesmichaelrobert@gmail.com>
2023-08-15 12:27:53 +0100

    Added parameter quantisation

--- experiment_batcher.py
+++ experiment_batcher.py
@@ -96,6 +96,7 @@ if __name__=="__main__":
                                 
                                 count = count + 1 
                                 
+                            ##
                         ##
                     ##
                 ##
